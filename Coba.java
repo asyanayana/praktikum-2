7e9d14e09f3bde18f7bc56519bd49493c51ea5cd
asyanayana <71356587+asyanayana@users.noreply.github.com>
2020-09-15 22:53:43 -0700

    Add files via upload

---
+++ Coba.java
@@ -0,0 +1,7 @@
+public class Coba {
+   public static void main (String [] args) {
+	System.out.println("Nama: Asyana!") ;
+	System.out.println("Nim: 6706201016") ;
+	System.out.println("Kelas: D3IF4402") ;
+    }
+}
